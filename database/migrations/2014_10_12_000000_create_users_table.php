1b91070535e882c8c227f8ceea5af97f70556be7
ayu <ayuanisan@gmail.com>
2019-12-12 18:27:07 +0700

    menambahkan migration database user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,14 +13,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('nama');
+            $table->string('email')->('unique');
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('no_hp');
+            $table->string('alamat');
+            $table->string('avatar');
+            $table->timestamps('created_at');
+            $table->timestamps('updated_at');
         });
     }
 
